8843cc994116d2a8fb84493269f0abede397ef60
ArturStahov <vtemy.at.ua@gmail.com>
2021-01-09 02:53:35 +0200

    complete v 1.0

---
+++ README_webpack.md
@@ -0,0 +1,9 @@
+## PIXI ShockwaveFilter effect
+
+use:
+
+- webpack;
+- pixi.js;
+- pixi-filter;
+
+view :
--- src/components/RenderPortal.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-
-export default function RenderPortal({ children, selectorDom }) {
-  const elemRoot = document.querySelector(selectorDom);
-  return ReactDOM.createPortal({ children }, elemRoot);
-}
